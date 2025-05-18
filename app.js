739f67585c4a7796388d3b896e7269f77254587d
AzizullahCodes <azizullahcodes@gmail.com>
2025-05-18 17:28:31 +0500

    u

--- app.js
+++ app.js
@@ -17,18 +17,18 @@
         // else if(enter_Marks >= 70){alert('Grade C');}
         // else{alert('Fail');}}
 
-        // ============= small-prctice-project-02(Assignment 3: Login System)=======================
+        // ============= small-prctice-project-03(Assignment 3: Login System)=======================
 
 //     var userName = prompt('Enter user name');
 //     var enter_Password = prompt('Enter password');
 //     var user = 'admin';
 //     var pass = '1234';
 //     if(enter_Password === pass){if(enter_Password === pass){alert('Login successful');}
-//  else if(enter_Password !== pass){alert('Incorrect password');}}
-// else{alert('unknown user');}
+//   else if(enter_Password !== pass){alert('Incorrect password');}}
+//  else{alert('unknown user');}
 
 
-// ============= small-prctice-project-02(Assignment 4: Age Group Checker)=======================
+// ============= small-prctice-project-04(Assignment 4: Age Group Checker)=======================
         // var age = prompt('Enter your age');
         // if (age >= '0' && age <= 120){if(age < 13){alert('child');}
         // else if (age < 20){alert('Teenager');}
@@ -37,3 +37,33 @@
 
 
         // else{alert('Invalid age');}
+
+        // ============= small-prctice-project-05(check vowel or constant without using any fuction or built in method)=======================
+        // var  lr = prompt('Enter a signal alphabet letter like a, b, m, etc');
+        // if (lr === 'a' ||lr === 'b' ||lr === 'c' ||lr === 'd' ||lr === 'c' ||lr === 'd' ||lr === 'e' ||lr === 'f' ||lr === 'g' ||lr === 'h' ||lr === 'i' ||lr === 'j' ||lr === 'k' ||lr === 'l' ||lr === 'm' ||lr === 'n' ||lr === 'o' ||lr === 'p' ||lr === 'q' ||lr === 'r' ||lr === 's' ||lr === 't' ||lr === 'u' ||lr === 'v' ||lr === 'w' ||lr === 'x' ||lr === 'y' ||lr === 'z' ){
+        //     if (lr === 'a' || lr === 'e' || lr === 'i' || lr === 'o' || lr === 'u'){alert('it is vowel');}
+        //     else {alert('it is constant');}
+        // }
+
+        // else{alert('Invalid entry');}
+
+        // ============= small-prctice-project-06(simple calculator)=======================
+//        
+
+    // var number1 = +prompt('Enter a number');
+    // var number2 = +prompt('Enter number 2');
+    // var operator = prompt('Enter an operator (+,-,*,/)');
+    // if (operator === '+' || operator === '-' || operator === '/' || operator === '*'){if (operator === '+'){alert(number1 + number2);}
+    // else if (operator === '-'){alert(number1 - number2);}
+    // else if (operator === '/'){alert(number1 / number2)}
+    // else if (operator === '*'){alert(number1 * number2);}
+        
+    // }
+    // else {alert('Invalid operator');}
+
+     // ============= small-prctice-project-07(Even or Odd and Greater than 100))=======================
+            // var enter_number = +prompt('Enter any number');
+            // var check = enter_number % 2;
+            // if (check === 0){if(check  === 0 && enter_number > 100){alert('Even and greater than 100');}
+            // else{alert('Even and less than 100')}}
+            // else{alert('No is odd');}
