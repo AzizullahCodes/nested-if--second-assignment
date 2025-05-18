d9522dc16f29fc7c3b6086dc04081fbbcf730152
AzizullahCodes <azizullahcodes@gmail.com>
2025-05-18 13:12:41 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+// nested if second prcatice assignment
+
+// ============= small-prctice-project-01(even or odd checker)=======================
+    //  var enter_number = +prompt('Enter any number');
+    // if (enter_number > 0){var check_even = enter_number % 2;
+    //      if(check_even === 0){console.log('no is even');}
+    //     else{alert('no is odd');}
+    // }
+    //  else{console.log('Number is negative or zero');
+        
+    //  }
+
+    // ============= small-prctice-project-02(Grade calculator)=======================
+        //     var enter_Marks = +prompt('Enter marks between 0 and 100');
+        //     if (enter_Marks >= 0 && enter_Marks <= 100){if(enter_Marks >= 90){alert('Grade A');}
+        // else if (enter_Marks >= 80){alert('Grade B');}
+        // else if(enter_Marks >= 70){alert('Grade C');}
+        // else{alert('Fail');}}
+
+        // ============= small-prctice-project-02(Assignment 3: Login System)=======================
+
+//     var userName = prompt('Enter user name');
+//     var enter_Password = prompt('Enter password');
+//     var user = 'admin';
+//     var pass = '1234';
+//     if(enter_Password === pass){if(enter_Password === pass){alert('Login successful');}
+//  else if(enter_Password !== pass){alert('Incorrect password');}}
+// else{alert('unknown user');}
+
+
+// ============= small-prctice-project-02(Assignment 4: Age Group Checker)=======================
+        // var age = prompt('Enter your age');
+        // if (age >= '0' && age <= 120){if(age < 13){alert('child');}
+        // else if (age < 20){alert('Teenager');}
+        // else if (age < 60){alert('adult');}
+        // else {alert('senior');}}
+
+
+        // else{alert('Invalid age');}
